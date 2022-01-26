9f1519339b5a9e7a89cac179bde6e0e3003e8ed1
Alec <prettale4@gmail.com>
2022-01-26 12:05:42 -0700

    changed html

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const path = require('path')
 require('dotenv').config()
-const port = process.env.PORT || process.env.SERVER_PORT
+const port = process.env.PORT
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-app.use(express.json())
 app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
 
 app.listen(port, () => {
